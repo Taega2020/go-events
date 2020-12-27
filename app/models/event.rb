2c4e0de5bc82f6497980579c00d07a294964389d
Teaga2020 <sand_bun2007@yahoo.co.jp>
2020-12-27 17:10:45 +0900

    eventモデル実装

---
+++ app/models/event.rb
@@ -0,0 +1,19 @@
+class Event < ApplicationRecord
+  with_options presence: true
+    validates :name   , length: {maximum: 50}
+    validates :place  , length: {maximum: 100}
+    validates :content, length: {maximum: 2000}
+    validates :start_at
+    validates :end_at
+    validates :start_at_should_be_before_end_at
+
+  private
+
+  def start_at_should_be_before_end_at
+    return unless start_at && end_at
+      
+    if start_at >= end_at
+      errors.add(:start_at, "は終了時間よりも前に設定してください")
+    end
+  end
+end
