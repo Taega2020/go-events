f1d9f2c2c8d1c86a1473d9f6b0cb77caa4739723
Teaga2020 <sand_bun2007@yahoo.co.jp>
2021-01-10 17:28:55 +0900

    devise導入からモデル、ビューの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admins
   root 'welcome#index'
   get "/auth/:provider/callback" => "sessions#create"
   delete "/logout" => "sessions#destroy", as: :logout
